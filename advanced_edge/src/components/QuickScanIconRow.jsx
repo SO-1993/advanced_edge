df4d0a25118f3d1a3ca14e75ddf654a8cbe9c6cf
Scott O'Brien <scottobrien93@outlook.com>
2026-01-19 14:41:02 +0000

    QuickScan: required content added (no styling)

---
+++ advanced_edge/src/components/QuickScanIconRow.jsx
@@ -0,0 +1,40 @@
+import '../css/App.css';
+import '../css/index.css';
+
+function QuickScan() {
+    return <div className="quick-scan">
+        <div className="basic-icons-row">
+            <p>🛠  Craftsmanship</p>
+            <p>📋 Standards</p>
+            <p>🤝 Professional</p>
+            <p>✨ Detail</p>
+        </div>
+        <div className="detailed-icons-row">
+            <h2>Why Choose Advanced Edge</h2>
+            <div className="craftsmanship-info">
+            <h5>🛠  Craftsmanship Built on Experience</h5>
+            <p>A 10-20 word description to be added here to flesh this out!</p>
+            </div>
+            <div className="standards-info">
+            <h5>📋  Uncompromising Standards</h5>
+            <p>A 10-20 word description to be added here to flesh this out!</p>
+            </div>
+            <div className="professional-info">
+            <h5>🤝  Professional, Transparent Service</h5>
+            <p>A 10-20 word description to be added here to flesh this out!</p>
+            </div>
+            <div className="detail-info">
+            <h5>✨  Pride in Detail & Finish</h5>
+            <p>A 10-20 word description to be added here to flesh this out!</p>
+            </div>
+
+
+        
+        </div>
+    </div> 
+
+       
+    
+}
+
+export default QuickScan;
--- advanced_edge/src/pages/HomePage.jsx
+++ advanced_edge/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../components/Hero";
+import QuickScan from "../components/QuickScanIconRow";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
+      <QuickScan />
     </div>
   );
 };
