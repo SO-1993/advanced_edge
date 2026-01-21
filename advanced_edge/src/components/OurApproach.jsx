0d31a69c1acce6359fd3002826897ede46805924
Scott O'Brien <scottobrien93@outlook.com>
2026-01-21 16:05:36 +0000

    first draft styling added to OurApproach component

--- advanced_edge/src/components/OurApproach.jsx
+++ advanced_edge/src/components/OurApproach.jsx
@@ -2,26 +2,32 @@ import '../css/App.css';
 import '../css/index.css';
 
 function OurApproach() {
-    return <div className="our-approach">
-        <h2>Our Approach</h2>
-        <div className="our-approach-row">
-            <h5>🗣️ Consultation</h5>
-            <p>A 5-10 word description to be added here to flesh this out!</p>
-            </div>
-            <div className="standards-info">
-            <h5>🧱 Preparation</h5>
-            <p>A 5-10 word description to be added here to flesh this out!</p>
-            </div>
-            <div className="professional-info">
-            <h5>🛠 Execution</h5>
-            <p>A 5-10 word description to be added here to flesh this out!</p>
-            </div>
-            <div className="detail-info">
-            <h5>✔ Completion</h5>
-            <p>A 5-10 word description to be added here to flesh this out!</p>
-            </div>
+  return (
+    <section className="our-approach">
+      <h2>Our Approach</h2>
+      <div className="approach-grid">
+        <div className="approach-item">
+          <h5>🗣️ Consultation</h5>
+          <p>Understanding requirements and setting clear expectations.</p>
+        </div>
 
-    </div>
+        <div className="approach-item">
+          <h5>🧱 Preparation</h5>
+          <p>Proper groundwork, drainage, and structural setup.</p>
+        </div>
+
+        <div className="approach-item">
+          <h5>🛠 Execution</h5>
+          <p>Precision work using proven methods and quality materials.</p>
+        </div>
+
+        <div className="approach-item">
+          <h5>✔ Completion</h5>
+          <p>Final clean-up and delivering a lasting, high-quality finish.</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default OurApproach;
