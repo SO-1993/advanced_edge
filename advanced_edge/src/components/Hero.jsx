a34fa5c9884ae9977726860078eb592068312cd0
Scott O'Brien <scottobrien93@outlook.com>
2026-01-22 14:24:28 +0000

    Hero: imported useNavigate; used it to navigate to gallery when 'View our Work' button is clicked

--- advanced_edge/src/components/Hero.jsx
+++ advanced_edge/src/components/Hero.jsx
@@ -1,14 +1,24 @@
 import '../css/App.css';
 import '../css/index.css';
 
+import { useNavigate } from 'react-router-dom';
+
+
 function HeroSection() {
+    const navigate = useNavigate();
+
+    const navigateToGallery = () => {
+        navigate('/gallery');
+      };
+
+
     return <div className="hero">
         <div>
             <h2>Refined groundworks, paving & landscaping</h2>
             <h4>built on precision, preparation, and lasting quality.</h4>
             <p>Serving homeowners who value craftsmanship, professionalism, and results that stand the test of time.</p>
             <button className="contact-us">Contact Us</button>
-            <button className="view-work">View our Work</button>
+            <button className="view-work" onClick={navigateToGallery}>View our Work</button>
             </div>
     </div> 
 
