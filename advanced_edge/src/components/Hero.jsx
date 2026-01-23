17c52e0664ca5f877ec50169533619e65e2d2a1b
Scott O'Brien <scottobrien93@outlook.com>
2026-01-23 09:42:13 +0000

    Hero image now displaying as background of Hero section

--- advanced_edge/src/components/Hero.jsx
+++ advanced_edge/src/components/Hero.jsx
@@ -2,7 +2,8 @@
 import '../css/App.css';
 import '../css/index.css';
 import { useNavigate } from 'react-router-dom'; // optional if using routes
-import Contact from './Contact';
+
+import hero_img from "../assets/images/hero_img.jpg";
 
 function HeroSection() {
     const navigate = useNavigate();
@@ -14,19 +15,22 @@ function HeroSection() {
     const scrollToContactForm = () => {
         const form = document.getElementById('contact-form');
         if (form) {
-        form.scrollIntoView({ behavior: 'smooth' }); // smooth scroll to form
+            form.scrollIntoView({ behavior: 'smooth' }); // smooth scroll to form
         }
     };
 
     return (
-        <div className="hero">
-        <div>
-            <h2>Refined groundworks, paving & landscaping</h2>
-            <h4>built on precision, preparation, and lasting quality.</h4>
-            <p>Serving homeowners who value craftsmanship, professionalism, and results that stand the test of time.</p>
-            <button className="contact-us" onClick={scrollToContactForm}>Contact Us</button>
-            <button className="view-work" onClick={navigateToGallery}>View our Work</button>
-        </div>
+        <div
+            className="hero"
+            style={{ backgroundImage: `url(${hero_img})` }}
+        >
+            <div>
+                <h2>Refined groundworks, paving & landscaping</h2>
+                <h4>built on precision, preparation, and lasting quality.</h4>
+                <p>Serving homeowners who value craftsmanship, professionalism, and results that stand the test of time.</p>
+                <button className="contact-us" onClick={scrollToContactForm}>Contact Us</button>
+                <button className="view-work" onClick={navigateToGallery}>View our Work</button>
+            </div>
         </div>
     );
 }
