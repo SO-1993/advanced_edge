088d26b98bfece40ab94da11c8b1fae8ae1933ef
Scott O'Brien <scottobrien93@outlook.com>
2026-01-23 16:06:05 +0000

    added dependencies, CNAME, etc, ran build in terminal

--- advanced_edge/vite.config.js
+++ advanced_edge/vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     dedupe: ['react', 'react-dom'], // Force all packages to use the same React
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'], // Force all packages to use the same React
-  },
-});
+  base: '/advanced_edge/',
+  plugins: [react()]
+})
