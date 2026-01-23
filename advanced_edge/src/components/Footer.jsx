71efe6b117cad7dd27e1cdf390da4910b6d739dd
Scott O'Brien <scottobrien93@outlook.com>
2026-01-23 10:45:01 +0000

    Company logo rendering on header + footer; acts as redirect to Homepage

--- advanced_edge/src/components/Footer.jsx
+++ advanced_edge/src/components/Footer.jsx
@@ -2,11 +2,15 @@ import { Link } from "react-router-dom";
 import '../css/App.css';
 import '../css/index.css';
 
+import new_logo from "../assets/images/new_logo.png";
+
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer_brand">
-        <Link to="/">Advanced Edge</Link>
+        <Link to="/">
+        <img src={new_logo} alt="Company logo" className="footer-logo" />
+        </Link>
       </div>
 
       <div /> {/* spacer column */}
--- advanced_edge/src/components/Header.jsx
+++ advanced_edge/src/components/Header.jsx
@@ -2,11 +2,17 @@ import { Link } from "react-router-dom";
 import '../css/App.css';
 import '../css/index.css';
 
+import new_logo from "../assets/images/new_logo.png";
+
 function Header() {
     return (
       <header className="header">
         <div className="header_brand">
-          <Link to="/">Advanced Edge</Link>
+          <Link to="/">
+          <Link to="/">
+              <img src={new_logo} alt="Company logo" className="header-logo" />
+            </Link>
+          </Link>
         </div>
   
         <div /> {/* spacer column */}
