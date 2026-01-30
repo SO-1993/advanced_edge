7325badb2f36adbe2dc5aefb5c0cec16a5e5010e
Scott O'Brien <scottobrien93@outlook.com>
2026-01-30 09:11:14 +0000

    QuickScan provided with react feather icons

--- advanced_edge/src/components/QuickScan.jsx
+++ advanced_edge/src/components/QuickScan.jsx
@@ -1,14 +1,36 @@
-import '../css/App.css';
-import '../css/index.css';
+import "../css/App.css";
+import "../css/index.css";
+
+import {
+  FiBriefcase,
+  FiStar,
+  FiClock,
+  FiZap
+} from "react-icons/fi";
 
 function QuickScan() {
   return (
     <section className="quick-scan">
       <div className="quick-scan-inner">
-        <p>150+ Projects Completed</p>
-        <p>5.0★ Average Rating</p>
-        <p>15+ Years Experience</p>
-        <p>24hr Response Time</p>
+        <div className="scan-item">
+          <FiBriefcase className="scan-icon" />
+          <p>150+ Projects Completed</p>
+        </div>
+
+        <div className="scan-item">
+          <FiStar className="scan-icon" />
+          <p>5.0 Average Rating</p>
+        </div>
+
+        <div className="scan-item">
+          <FiClock className="scan-icon" />
+          <p>15+ Years Experience</p>
+        </div>
+
+        <div className="scan-item">
+          <FiZap className="scan-icon" />
+          <p>24hr Response Time</p>
+        </div>
       </div>
     </section>
   );
