b1f672e3622a9236f9561ad4013fa22890078367
Scott O'Brien <scottobrien93@outlook.com>
2026-02-02 12:31:52 +0000

    added ScrollToTop component so that redirects are always to top of the page

--- advanced_edge/src/App.jsx
+++ advanced_edge/src/App.jsx
@@ -2,18 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/Homepage";
 import GalleryPage from "./pages/GalleryPage";
+import ScrollToTop from "./components/ScrollToTop";
+import Footer from "./components/Footer"; // if you have one
 
 function App() {
   return (
-    <div>
-      <Header/>
+    <>
+      <ScrollToTop />
+      <Header />
+
       <main className="main-content">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/gallery" element={<GalleryPage />} />
         </Routes>
       </main>
-      </div>
+      <Footer /> {/* if you’re using it */}
+    </>
   );
 }
 
